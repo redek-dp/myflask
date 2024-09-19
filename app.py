5c41148e6a9a3decb5ec2271650f18e3fa080aa2
Redek-dp <41121772+redek-dp@users.noreply.github.com>
2024-09-19 15:59:21 -0300

    1.1

--- app.py
+++ app.py
@@ -1,11 +1,34 @@
-from flask import Flask, render_template
+#from flask import Flask, render_template
 
-app = Flask(__name__)
+#app = Flask(__name__)
+
+#@app.route('/')
+#def index():
+#    return render_template('index.html')
+
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+
+import requests
+import json
+
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+url='https://jsonplaceholder.typicode.com/users'
+
+req=requests.get(url)
+json_form=req.json()
+
+
+#@app.route("/")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
-
+    return render_template('index.html',data=json_form)
 
 if __name__ == '__main__':
     app.run(debug=True)
